d443d5f8356f65efcc513c3a4477fcc5345f2b41
LihueelM <lihueel.m@hotmail.com>
2022-11-07 15:03:41 -0300

    Fixed images

--- components/Works.jsx
+++ components/Works.jsx
@@ -2,7 +2,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
-import Proyectos from '../public/works'
 
 
 const Works = () => {
@@ -10,20 +9,31 @@ const Works = () => {
     <div id='works'>
       <Container>
         <h2 className='title'>Mis proyectos</h2>
-        <Row>
-          {
-            Proyectos.map(e => {
-              return(
-                <Col className='md={2} sm={6} xs={12} d-flex flex-column col-works' key={e.name}>
-                  <h2>{e.name}</h2>
-                  <Link href={e.link} alt='proyecto' target='_blank'>                    
-                    <Image src={e.image} alt={e.name} width={380} height={240} className='img-works '/>                    
-                  </Link>         
-                  <h5>{e.description}</h5>         
-                </Col>
-              )
-            })
-          }
+        <Row> 
+          <Col className='md={2} sm={6} xs={12} d-flex flex-column col-works'>
+            <h2>BookStrap</h2>
+            <Link href='"https://github.com/LihueelM/BookStore-react"' alt='proyecto' target='_blank'>                    
+              <Image src='/../public/assets/bookstore.png' alt='bookstore' width={380} height={240} className='img-works '/>                    
+            </Link>         
+            <h5>Proyecto E-commerce realizado con React.js y Firebase</h5>         
+          </Col> 
+
+          <Col className='md={2} sm={6} xs={12} d-flex flex-column col-works'>
+            <h2>Critical Games</h2>
+            <Link href="https://github.com/LihueelM/BookStore-react" alt='proyecto' target='_blank'>                    
+              <Image src="/../public/assets/critical-games.png" alt='critical-games' width={380} height={240} className='img-works '/>                    
+            </Link>         
+            <h5>Proyecto realizado con HTML5, CSS3 y Bootstrap</h5>         
+          </Col> 
+
+          <Col className='md={2} sm={6} xs={12} d-flex flex-column col-works'>
+            <h2>Orwell Drinks</h2>
+            <Link href="https://github.com/LihueelM/Ecommerce---Javascript-Vanilla" alt='proyecto' target='_blank'>                    
+              <Image src="/../public/assets/drinks.png" alt='drinks orwell' width={380} height={240} className='img-works '/>                    
+            </Link>         
+            <h5>Proyecto E-commerce realizado con HTML5, CSS3 y JavaScript ES6</h5>         
+          </Col>
+         
         </Row>
       </Container>
     </div>
