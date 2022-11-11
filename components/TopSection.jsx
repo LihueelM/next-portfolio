a2fc7f9e73560f2a21bc9f105912ea570b2c8c82
Lihueel Mansilla <104211361+LihueelM@users.noreply.github.com>
2022-11-11 07:55:29 -0300

    Update TopSection.jsx

--- components/TopSection.jsx
+++ components/TopSection.jsx
@@ -15,8 +15,8 @@ const TopSection = () => {
               <Col md={8} xs={12} className='card-top'>                
                 <h1 className='titulo'>Hola! Mi nombre es Lihueel Mansilla</h1>
                 <h2 className='subtitulo'>Front-end developer</h2>
-                <h4>Actualmente estudiante de la carrera <p className='resaltado'>Licenciatura en sistemas</p>  en la universidad nacional general sarmiento.
-                  De forma complementaria con el objetivo de poder insertarme laboralmente curso paralelamente la carrera <p className='resaltado'>Full-stack</p> en CoderHouse</h4>
+                <h4>Actualmente estudiante de la carrera <span className='resaltado'>Licenciatura en sistemas</span>  en la universidad nacional general sarmiento.
+                  De forma complementaria con el objetivo de poder insertarme laboralmente curso paralelamente la carrera <span className='resaltado'>Full-stack</span> en CoderHouse</h4>
                 <Link href='https://github.com/LihueelM' target="_blank" alt='button'><Button variant="outline-warning">Github</Button></Link>                
               </Col>
               <Col md={4} xs={12} className='img-one' >
